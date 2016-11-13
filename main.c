52ceb76a179be177934f5c3a2b75f859f6f845fa
Robin Degen <robindegen@gmail.com>
2016-11-13 18:37:43 +0100

    Example code of how to use git describe from cmake for use in your own code!

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "buildinfo.h"
 
 int main(int argc, char *argv[])
 {
+    printf("Version: %s Build on: %s\n", GIT_DESCRIBE_VERSION, BUILD_DATE);
     printf("Hello world!\n");
     return 0;
 }
