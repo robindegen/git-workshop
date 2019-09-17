9be94e3566905cf4c96924c11bd534402126f02e
Robin Degen <robindegen@gmail.com>
2019-09-17 07:23:24 +0200

    Try to force an isolate conflict

--- new_file.c
+++ new_file.c
@@ -1,8 +1,13 @@
 // This is a source file!
 
+void AnotherFunction2()
+{
+    DoSomeStuff();
+}
+
 int MyAmazingFunction(int value)
 {
-    if (value == 1337)
+    if (value == 1337 && another_value = 1338)
     {
         return 0;
     }
